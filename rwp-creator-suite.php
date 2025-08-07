8859f36b614e5167c3d0526eac73cbf539c84179
Jeremy <jeremy@firecrown.com>
2025-08-07 11:06:09 -0400

    Adding base plugin file and git workflow

---
+++ rwp-creator-suite.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: RWP Creator Suite
+ * Description: A suite of tools for content creators.
+ * Version: 1.0.0
+ * Author: Jeremy Hixon
+ * Author URI: https://jeremyhixon.com
+ */
